e781dd1fef6d6964529edef75221269ac144e756
Bernardo <bernardoduarte271@gmail.com>
2025-01-06 16:04:44 -0300

    dropout layer

---
+++ test.py
@@ -0,0 +1,83 @@
+#Dropout layer 
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+
+def normalizer(image, label):
+    aux = tf.cast(image, dtype=tf.float32)
+    image_norm = aux/255.0
+    return image_norm, label
+
+train_data = tf.keras.utils.image_dataset_from_directory(
+    'dataset/training',
+    validation_split=0.1,  
+    subset="training",     
+    seed=42,               
+    image_size=(224, 224), 
+    batch_size=32          
+)
+
+valid_data = tf.keras.utils.image_dataset_from_directory(
+    'dataset/training',
+    validation_split=0.1,  
+    subset="validation",   
+    seed=42,               
+    image_size=(224, 224), 
+    batch_size=32          
+)
+
+train = train_data.map(normalizer)
+valid = valid_data.map(normalizer) 
+
+model = Sequential()
+
+model.add(Conv2D(filters=8, kernel_size=(3,3), activation='relu', input_shape=(224,224,3)))
+model.add(MaxPooling2D())
+
+model.add(Conv2D(filters=16, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D())
+
+model.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D())
+
+model.add(Flatten())
+
+model.add(Dropout(0.5))
+model.add(Dense(units=64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(units=1, activation='sigmoid'))
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    metrics=['accuracy'],
+)
+
+print(model.summary())
+
+hist = model.fit(
+    train,
+    batch_size=32, 
+    epochs=20, 
+    validation_data=valid
+)
+
+plt.plot(hist.history['loss'], label='train')
+plt.plot(hist.history['val_loss'], label='valid')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.title('Loss in Training and Validation')
+plt.show()
+
+if 'accuracy' in hist.history:  # Verifica se foi usada a métrica R²
+    plt.plot(hist.history['accuracy'], label='train')
+    plt.plot(hist.history['val_accuracy'], label='valid')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.title('Accuracy in Training and Validation')
+    plt.show()
