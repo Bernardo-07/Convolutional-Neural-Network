1f551eb9a800e30baffd5ac0831dca0aadc3b237
Bernardo <bernardoduarte271@gmail.com>
2025-01-27 13:32:29 -0300

    new test

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-#Dropout layer 
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization
+from tensorflow.keras.callbacks import EarlyStopping
 
 def normalizer(image, label):
     aux = tf.cast(image, dtype=tf.float32)
@@ -16,7 +16,7 @@ train_data = tf.keras.utils.image_dataset_from_directory(
     validation_split=0.1,  
     subset="training",     
     seed=42,               
-    image_size=(224, 224), 
+    image_size=(256, 256), 
     batch_size=32          
 )
 
@@ -25,7 +25,7 @@ valid_data = tf.keras.utils.image_dataset_from_directory(
     validation_split=0.1,  
     subset="validation",   
     seed=42,               
-    image_size=(224, 224), 
+    image_size=(256, 256), 
     batch_size=32          
 )
 
@@ -34,20 +34,14 @@ valid = valid_data.map(normalizer)
 
 model = Sequential()
 
-model.add(Conv2D(filters=8, kernel_size=(3,3), activation='relu', input_shape=(224,224,3)))
+model.add(Conv2D(filters=8, kernel_size=(3,3), activation='relu', input_shape=(256,256,3)))
 model.add(MaxPooling2D())
-model.add(Dropout(0.25))
-model.add(BatchNormalization())
 
-model.add(Conv2D(filters=16, kernel_size=(3,3), activation='relu'))
+model.add(Conv2D(filters=16, kernel_size=(3,3), activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
 model.add(MaxPooling2D())
-model.add(Dropout(0.25))
-model.add(BatchNormalization())
 
-model.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu'))
+model.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)))
 model.add(MaxPooling2D())
-model.add(Dropout(0.25))
-model.add(BatchNormalization())
 
 model.add(Flatten())
 
@@ -64,11 +58,15 @@ model.compile(
 
 print(model.summary())
 
+early_stopping = EarlyStopping(monitor='val_loss', patience=4, restore_best_weights=True)
+
 hist = model.fit(
     train,
     batch_size=32, 
     epochs=20, 
-    validation_data=valid
+    validation_data=valid,
+    callbacks=[early_stopping], 
+    verbose=1
 )
 
 plt.plot(hist.history['loss'], label='train')
