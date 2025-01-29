2d058ff53bd21821ca5e49c7da4a2fda4feeb613
Bernardo <bernardoduarte271@gmail.com>
2025-01-29 14:56:33 -0300

    model predict

--- test.py
+++ test.py
@@ -87,3 +87,128 @@ if 'accuracy' in hist.history:
     plt.legend()
     plt.title('Accuracy in Training and Validation')
     plt.show()
+    
+    
+    
+    
+    
+import tensorflow as tf
+import numpy as np
+import pandas as pd  
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range = 15,
+    horizontal_flip = True,
+    zoom_range = 0.2,
+    shear_range = 0.1,
+    fill_mode = 'reflect',
+    width_shift_range = 0.1,
+    height_shift_range = 0.1,
+    validation_split = 0.15
+)
+
+# Create data loaders
+train_data = train_datagen.flow_from_directory(
+    'dataset/training',
+    target_size=(128, 128),      
+    batch_size=32,
+    class_mode='binary',
+    subset='training'
+)
+
+valid_data = train_datagen.flow_from_directory(
+    'dataset/training',
+    target_size=(128, 128),
+    batch_size=32,
+    class_mode='binary',
+    subset='validation'
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+test_data = test_datagen.flow_from_directory(
+    'dataset/test',
+    target_size=(128, 128),
+    batch_size=32,
+    class_mode='binary',
+    subset='test'
+)
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, BatchNormalization, Dropout
+
+model = Sequential()
+
+# Input Layer
+model.add(Conv2D(32,(3,3),activation='relu',input_shape = (128,128,3))) 
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Bloack 1 
+model.add(Conv2D(64,(3,3),activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+# Block 2
+model.add(Conv2D(128,(3,3),activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+# Block 3
+model.add(Conv2D(256,(3,3),activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Fully Connected layers 
+model.add(Flatten())
+model.add(Dense(512,activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+# Output layer
+model.add(Dense(2,activation='softmax'))
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    metrics=['accuracy'],
+)
+
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+
+learning_rate_reduction = ReduceLROnPlateau(
+    monitor = 'val_accuracy',
+    patience=2,
+    factor=0.5,
+    min_lr = 0.00001,
+    verbose = 1
+)
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+
+hist = model.fit(
+    train_data,
+    batch_size=32, 
+    epochs=30, 
+    validation_data=valid_data, 
+    callbacks=[early_stopping,learning_rate_reduction],
+    verbose= 1
+)
+
+# prediction
+result = model.predict(test_data,batch_size = 32,verbose = 0)
+
+y_pred = np.argmax(result, axis = 1)
+
+y_true = test_data.labels
+
+# Evaluvate
+loss,acc = model.evaluate(test_data, batch_size = 32, verbose = 0)
+
+print('The accuracy of the model for testing data is:',acc*100)
+print('The Loss of the model for testing data is:',loss)
