cff58dd10c6f91fed9fac8c6ee71b8c4b733e62a
Bernardo <bernardoduarte271@gmail.com>
2025-01-31 10:09:08 -0300

    changes on cnn

--- test.py
+++ test.py
@@ -4,27 +4,59 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+import os
+import imghdr
 
-def normalizer(image, label):
-    image = augmentation(image) 
+def normalizer(image, label): 
     aux = tf.cast(image, dtype=tf.float32)
     image_norm = aux/255.0
     return image_norm, label
 
-augmentation = tf.keras.Sequential([
-    tf.keras.layers.RandomFlip("horizontal"),
-    tf.keras.layers.RandomRotation(0.1),
-    tf.keras.layers.RandomZoom(0.1),
-    tf.keras.layers.RandomBrightness(0.1)
-])
+dir = "dataset/training"
+
+datagen = ImageDataGenerator(
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+for class_name in os.listdir(dir):  
+    class_path = os.path.join(dir, class_name)
+
+    for img_name in os.listdir(class_path):
+        img_path = os.path.join(class_path, img_name)
+
+        if imghdr.what(img_path) not in ['jpeg', 'png', 'jpg']:  # Verifica se é uma imagem
+            print(f"Arquivo não suportado ou corrompido: {img_name}")
+            continue
+    
+        try:
+            img = load_img(img_path)
+            img_array = img_to_array(img)
+            img_array = np.expand_dims(img_array, axis=0)
+            
+            count = 0
+            for batch in datagen.flow(img_array, batch_size=1, save_to_dir=class_path, save_prefix=f"aug_{img_name.split('.')[0]}", save_format="jpg"):
+                count += 1
+                if count >= 2: 
+                    break
+        except Exception as e:
+            print(f"Erro ao carregar {img_name}: {e}")
+            continue
 
 train_data = tf.keras.utils.image_dataset_from_directory(
     'dataset/training',
-    validation_split=0.1,  
+    validation_split=0.15,  
     subset="training",     
     seed=42,               
-    image_size=(224, 224), 
+    image_size=(128, 128),
+    shuffle=True, 
     batch_size=32          
 )
 
@@ -33,7 +65,7 @@ valid_data = tf.keras.utils.image_dataset_from_directory(
     validation_split=0.1,  
     subset="validation",   
     seed=42,               
-    image_size=(224, 224), 
+    image_size=(128, 128), 
     batch_size=32          
 )
 
@@ -42,26 +74,34 @@ valid = valid_data.map(normalizer)
 
 model = Sequential()
 
-model.add(Conv2D(filters=16, kernel_size=(3,3), activation='relu', input_shape=(224,224,3)))
+model.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu', input_shape=(128,128,3)))
 model.add(BatchNormalization())
 model.add(MaxPooling2D())
-
-model.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D())
+model.add(Dropout(0.2))
 
 model.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D())
-model.add(Dropout(0.5))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(filters=128, kernel_size=(3,3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D())
+model.add(Dropout(0.2))
+
+model.add(Conv2D(filters=256, kernel_size=(3,3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D())
+model.add(Dropout(0.2))
 
 model.add(Flatten())
 
-model.add(Dense(units=128, activation='relu'))
+model.add(Dense(units=512, activation='relu'))
 model.add(BatchNormalization())
 model.add(Dropout(0.5))
 
 model.add(Dense(units=1, activation='sigmoid'))
+
 
 model.compile(
     optimizer='adam',
@@ -71,15 +111,24 @@ model.compile(
 
 print(model.summary())
 
-#early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+learning_rate_reduction = ReduceLROnPlateau(
+    monitor = 'val_accuracy',
+    patience=2,
+    factor=0.5,
+    min_lr = 0.00001,
+    verbose = 1
+)
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True, verbose= 0)
 
 hist = model.fit(
-    train,
-    batch_size=32, 
-    epochs=30, 
-    validation_data=valid
-    #callbacks=[early_stopping], 
-    #verbose=1
+    train_data,
+    validation_data=valid_data, 
+    epochs=30,  
+    batch_size=32,
+    shuffle=True,
+    callbacks=[early_stopping,learning_rate_reduction],
+    verbose = 1
 )
 
 plt.plot(hist.history['loss'], label='train')
